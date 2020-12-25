f9e77ff5a92f21ec44e020cac1c594ad41435222
Mahler, Thomas <thomas.mahler@ista.com>
2020-12-25 21:48:51 +0100

    let's have an exists property in QuickCheck.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,65 +1,23 @@
 module Lib where
 
-import Linear ((^+^))
-import Linear.V1
-import Linear.V2
-import Linear.V3
-import Linear.V4
-import Linear.Matrix ((!*!), (!*), (*!))
-import Linear.Vector (zero)
-
 import           Control.Parallel (pseq)
 import           Control.Parallel.Strategies (rseq, using, parMap)
 
-position = V2 1 0
-
-vec = V3 2 2 2
-
-
-
-simpleMapReduce ::
-     (a -> b)   -- map function
+simpleMapReduce 
+  :: (a -> b)   -- map function
   -> ([b] -> c) -- reduce function
   -> [a]        -- list to map over
   -> c          -- result
 simpleMapReduce mapFunc reduceFunc = reduceFunc . map mapFunc
 
-parMapReduce :: (a -> b) -> ([b] -> c) -> [a] -> c
+parMapReduce 
+  :: (a -> b)   -- map function
+  -> ([b] -> c) -- reduce function
+  -> [a]        -- list to map over
+  -> c          -- result
 parMapReduce mapFunc reduceFunc input =
     mapResult `pseq` reduceResult
     where mapResult    = parMap rseq mapFunc input
           reduceResult = reduceFunc mapResult `using` rseq
 
-
-matrixReduce :: [V3 (V3 Integer)] -> V3 (V3 Integer)
-matrixReduce = foldr (!*!) one
-  where one = V3 (V3 1 1 1) (V3 1 1 1) (V3 1 1 1)
-
-
-someFunc :: IO ()
-someFunc = do
-  let a = V3 (V3 1 2 3) (V3 2 3 4) (V3 3 4 5)
-      --a' = V4 (V3 1 2 3) (V3 2 3 4) (V3 3 4 5) (V3 4 5 6)
-      b = V3 (V3 2 3 5) (V3 7 11 13) (V3 17 19 23)
-      
-      m = simpleMapReduce id matrixReduce [a,b,a,b] 
-      m' = simpleMapReduce id matrixReduce [b,a,b,a] 
-      
-      m'' = parMapReduce id matrixReduce [a,b,a,b] 
-      m''' = parMapReduce id matrixReduce [a,b,a,b] 
-      
---      c = a !*! b :: V4 (V4 Integer)
---      c' = b !*! a
---      d = V4 1 2 3 4 :: (V4 Integer)
---      e = c !* d
---      f = d *! c
---      g = V4 (V2 2 3) (V2 4 5) (V2 6 7) (V2 8 9)
---      h = (a !*! b) !*! g
---      i = a !*! (b !*! g)
---      --j = (b !*! a) !*! g
-
-  print m
-  print m'
-  print m''
-  print m'''
 
--- test/ComMonSpec.hs
+++ test/ComMonSpec.hs
@@ -1,34 +1,46 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module ComMonSpec where
 
 import           Test.Hspec            hiding (it)
 import           TestUtils        (it)
 import           Test.QuickCheck
-
-import Linear
+import           Prelude hiding (concat)
 import Lib
+import qualified Test.QuickCheck.Property as P (reason, callbacks, mapResult)
+import GHC.Natural (Natural)
 
-instance Arbitrary (V3 (V3 Integer)) where
-    arbitrary = do
-      v1 <- arbitrary
-      v2 <- arbitrary
-      v3 <- arbitrary
-      v4 <- arbitrary
-      v5 <- arbitrary
-      v6 <- arbitrary
-      v7 <- arbitrary
-      v8 <- arbitrary
-      v9 <- arbitrary
-      
-      return $ V3 (V3 v1 v2 v3) (V3 v4 v5 v6) (V3 v7 v8 v9)
+concat :: String -> String -> String
+concat a b = a ++ b
+
+--forAll :: (Show a, Testable prop)
+--       => Gen a -> (a -> prop) -> Property
+
+exists :: (Show a, Arbitrary a) => (a -> Bool) -> Property
+exists pred = disjoin $ replicate 100 $ forAll (resize 500 arbitrary) pred
+
+take5 :: String -> String
+take5 = take 5
+
+
 
 spec :: Spec
 spec =
-  describe "commutative Monoid" $ do
+  describe "String concat Monoid" $ do
+    
+    it "is associative" $ 
+      property $ \a b c -> (((a ++ b) ++ c) :: String) `shouldBe` (a ++ (b ++ c))
 
-    it "parallel and sequential evaluation return the same result" $
-      property $ \a b c d e f g h j k l m o p q r s t u v w x y z-> parMapReduce id matrixReduce [a, b, c, d,e, f, g, h, j, k, l, m, o, p, q, r, s, t, u, v, w, x, y, z]  `shouldBe` simpleMapReduce id matrixReduce [a, b, c, d,e, f, g, h, j, k, l, m, o, p, q, r, s, t, u, v, w, x, y, z]
+    it "is not commutative" $
+      exists $ \((x,y) :: (String, String)) -> x ++ y /= y ++ x
+
+
+
+--    it "parallel and sequential evaluation return the same result" $
+--      property $ \a b c d e f g h j k l m o p q r s t u v w x y z 
+--        -> (parMapReduce id concat [a, b, c, d, e, f, g, h, j, k, l, m, o, p, q, r, s, t, u, v, w, x, y, z]  )
+--        `shouldBe` (simpleMapReduce id concat [a, b, c, d, e, f, g, h, j, k, l, m, o, p, q, r, s, t, u, v, w, x, y, z])
 
 
 
@@ -37,20 +49,7 @@ spec =
 --
 --      m'' = parMapReduce id matrixReduce [a,b,a,b]
 
---    it "computes the manhattanDistance of 0.0-1.1" $
---     manhattanDistance(Point 0 0, Point 1 1) === 2
---
---    it "computes the manhattanDistance of 1.1-1.2" $
---     manhattanDistance(Point 1 1, Point 1 2) `shouldBe` 1
---
---    it "computes the manhattanDistance of 1.1-2.2" $
---     manhattanDistance(Point 1 1, Point 2 2) `shouldBe` 2
---
---    it "computes the manhattanDistance of 2.2-5.4" $
---     manhattanDistance(Point 2 2, Point 5 4) === 5
---
---    it "computes the manhattanDistance of 5.4-2-2" $
---     manhattanDistance(Point 5 4, Point 2 2) === 5
+
 
 
 
