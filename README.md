efac11fae88ac4756e1c23646582ce4e7d1ba691
thma <gH1710>
2021-01-30 15:08:47 +0100

    some updates

--- README.md
+++ README.md
@@ -158,6 +158,10 @@ But it is not commutative *in general* – that is for all possible arguments.
 We could rephrase this property as *"There exists at least one pair of arguments
 for which `(⊕)` is not commutative"*.
 
+<p><span class="math display">\[\exists (x,y) \left [  x \oplus y \neq y \oplus x \right ] 
+  \equiv 
+  \neg \forall (x,y) \left [ x \oplus y = y \oplus x \right ]\]</span></p>
+
 QuickCheck does not come with a mechanism for *existential quantification*. 
 But as is has `forAll` that is *universal quantification*. So we can build our own
 tool for existential quantification 
--- test/MonoidSpec.hs
+++ test/MonoidSpec.hs
@@ -1,14 +1,16 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module MonoidSpec where
 
-import Lib
-
-import           Test.Hspec            --hiding (it)
+--hiding (it)
 -- import           TestUtils             (it)
+
+import           GHC.Natural              (Natural, naturalFromInteger)
+import           Lib
+import           Test.Hspec
 import           Test.QuickCheck
-import GHC.Natural (Natural, naturalFromInteger)
+import           Test.QuickCheck.Property as P
 
 instance Arbitrary Natural where
   arbitrary = do
@@ -16,55 +18,39 @@ instance Arbitrary Natural where
     return $ naturalFromInteger nonNegative
 
 --see https://stackoverflow.com/questions/42764847/is-there-a-there-exists-quantifier-in-quickcheck
-exists :: (Show a, Arbitrary a) 
-       => (a -> Bool) -> Property
-exists = forSome $ resize 100 arbitrary
-    
-forSome :: (Show a, Testable prop)
-        => Gen a -> (a -> prop) -> Property
-forSome gen prop = once $ disjoin $ replicate 100 $ forAll gen prop
+exists :: (Show a, Arbitrary a) => (a -> Bool) -> Property
+exists = forSome $ resize 1000 arbitrary
 
--- exists :: (Show a, Arbitrary a) 
---         => (a -> Bool) -> Property
--- exists prop = exists' (resize 10000 arbitrary) prop
-
--- exists' :: Gen a -> (a -> Bool) -> Property
--- exists' gen prop = property (exists'' 10000 gen prop)
-
--- exists'' :: Int -> Gen a -> (a -> Bool) -> Gen Bool
--- exists'' 0 _ _ = return False
--- exists'' n gen prop = do
---   a <- gen
---   if prop a
---     then return True
---     else exists'' (n - 1) gen prop
+forSome :: (Show a, Testable prop) => Gen a -> (a -> prop) -> Property
+forSome gen prop =
+  mapResult (\r -> r {P.reason = "No witness found.", P.callbacks = []}) $
+    once $ disjoin $ replicate 1000 $ forAll gen prop
 
 spec :: Spec
 spec = do
   describe "The Monoid 'Natural Numbers under Addition'" $ do
     it "is associative" $
       property $ \x y z -> ((x + y) + z) `shouldBe` ((x + (y + z)) :: Natural)
-      
+
     it "has 0 as left and right identity element" $
       property $ \x -> (x + 0 `shouldBe` (x :: Natural)) .&&. (0 + x `shouldBe` x)
-      
+
     it "is commutative" $
       property $ \x y -> x + y `shouldBe` (y + x :: Natural)
 
     it "is commutative (via forAll)" $
       forAll (resize 10000 arbitrary) $ \(x, y) -> x + y == (y + x :: Natural)
-    
---    it "is not commutative (via exists)" $
---      exists $ \(x, y) -> x + y /= (y + x :: Natural)
-  
-  
-  describe "The Monoid 'Strings under concatenation'" $ do    
-    it "is associative" $ 
+
+    it "is NOT commutative (via exists)" $
+      exists $ \(x, y) -> x + y /= (y + x :: Natural)
+
+  describe "The Monoid 'Strings under concatenation'" $ do
+    it "is associative" $
       property $ \x y z -> ((x ⊕ y) ⊕ z) `shouldBe` (x ⊕ (y ⊕ z))
-      
+
     it "has \"\" as left and right identity element" $
       property $ \x -> (x ⊕ "" `shouldBe` x) .&&. ("" ⊕ x `shouldBe` x)
-      
+
     --    it "is NOT commutative" $
     --      property $ \x y -> x ⊕ y `shouldNotBe` y ⊕ x
     --
@@ -73,17 +59,19 @@ spec = do
     --                          ==> (x ⊕ y) /= (y ⊕ x)
 
     it "is not commutative (via exists)" $
-      exists $ \(x,y) -> x ⊕ y /= y ⊕ x
+      exists $ \(x, y) -> x ⊕ y /= y ⊕ x
 
     it "works correctly with a sequential map-reduce" $
-      property $ \a b c d -> simpleMapReduce reverse (foldr (⊕) "") [a,b,c,d]
-                      `shouldBe` reverse a ⊕ reverse b ⊕ reverse c ⊕ reverse d
+      property $ \a b c d ->
+        simpleMapReduce reverse (foldr (⊕) "") [a, b, c, d]
+          `shouldBe` reverse a ⊕ reverse b ⊕ reverse c ⊕ reverse d
 
     it "has some cases where parallel reduction deviates from sequential reduction" $
-      exists $ \() -> parMapReduce reverse (foldr (⊕) "") text1
-                /= simpleMapReduce reverse (foldr (⊕) "") text1
+      exists $ \() ->
+        parMapReduce reverse (foldr (⊕) "") text1
+          /= simpleMapReduce reverse (foldr (⊕) "") text1
 
     it "parallel reduction always equals sequential reduction" $
-      property $ \a b c d -> simpleMapReduce reverse (foldr (⊕) "") [a,b,c,d]
-                      `shouldBe` parMapReduce reverse (foldr (⊕) "") [a,b,c,d]
-
+      property $ \a b c d ->
+        simpleMapReduce reverse (foldr (⊕) "") [a, b, c, d]
+          `shouldBe` parMapReduce reverse (foldr (⊕) "") [a, b, c, d]
